0415e93c0cd11f9333502f0fa6e52a710b9e8ffd
jm-maniego <jmmaniego@hotmail.com>
2017-01-12 16:31:22 +0800

    rails g react:install

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,9 @@
 //= require underscore
 //= require backbone
 //= require quiz_master
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
