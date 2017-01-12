62c55ef5caf2605852025d51ea60e3da50d0bd76
jm-maniego <jmmaniego@hotmail.com>
2017-01-12 15:43:40 +0800

    rails g scaffold quiz name:string

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'quizzes#index'
+  resources :quizzes
+  resources :questions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
